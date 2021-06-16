9c940787fa886b3785357a94bc143d1be91a6c21
Sócrates López <josesocrateslopez@gmail.com>
2021-06-16 15:02:18 -0500

    funcionalidad responsive faltante

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
-import 'materialize-css/dist/css/materialize.min.css'
+//import 'materialize-css/dist/css/materialize.min.css'
 import "./scss/style.scss";
