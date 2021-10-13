58643e49a45eac349a27eb4ba8d51f99cfa38ef5
Sócrates López <josesocrateslopez@gmail.com>
2021-10-12 19:28:26 -0500

    botón de descarga href automatico a ultima versión

--- src/js/ocs.js
+++ src/js/ocs.js
@@ -1,2 +1,16 @@
 import './common'
 import './../scss/ocs.scss'
+
+
+fetch("https://api.github.com/repos/KernelWar/OneConnection-server/releases/latest")
+.then(function(response){
+    var element = document.getElementById("link-download")    
+    response.json().then(data=>{
+        if(data.assets){            
+            element.href = data.assets[1].browser_download_url
+        }else{
+            document.getElementById("btn-download").innerText = "De momento no hay una versión disponible"
+            element.href = "https://github.com/KernelWar/OneConnection-server/releases"
+        }
+    })
+})
